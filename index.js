67910099ba0db407414e5d5ad6439c76bee4841f
AlexWellsHS <alex@headspace.com>
2019-10-28 00:25:41 -0700

    a few endpoints

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import bodyParser from 'body-parser';
 import express from 'express';
 import mongoist from 'mongoist';
 
@@ -5,7 +6,20 @@ const app = express();
 const port = 3000;
 const db = mongoist('mongodb://localhost:27017/events');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/events', async (req, res) => {
+  const events = await db.events.find();
+  res.send(events);
+});
+
+app.post('/point', async (req, res) => {
+  const point = req.body;
+  console.log(point);
+  // await db.points.insertOne(point);
+  res.send();
+});
 
 app.listen(port, async () => {
   console.log(`Example app listening on port ${port}!`);
