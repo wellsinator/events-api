422a1a8394f0e3b9f52311f3428ad7fe8f19657f
AlexWellsHS <alex@headspace.com>
2019-10-30 02:42:44 -0700

    use mysql instead of mongo

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 import bodyParser from 'body-parser';
 import express from 'express';
-import mongoist from 'mongoist';
+import db from './db/index';
 
 const app = express();
 const port = 3000;
-const db = mongoist('mongodb://localhost:27017/events');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/events', async (req, res) => {
-  const events = await db.events.find();
+  const events = await db.Event.findAll();
   res.send(events);
 });
 
 app.post('/point', async (req, res) => {
   const point = req.body;
-  await db.points.insertOne(point);
+  await db.Point.create(point);
   res.send();
 });
 
